02b6c3d6b215e4c5565ee009a8fd59de0f2b90f8
Mahbub Hasan Imon <mahbubhasanimon@gmail.com>
2021-01-09 19:38:59 +0600

    Added &display=swap
    Assalamu Alaikum,
    Hope you are well. I am using your font picker with some modifications. I think you should add &display=swap
    
    I will try to contribute again if I add new fonts :) and any problem if I use your font picker with some modifications? 
    
     Thanks <3

--- util/typography-control/FontPicker.js
+++ util/typography-control/FontPicker.js
@@ -37,7 +37,7 @@ function FontFamilyPicker({
     const meta = wp.data.select("core/editor").getEditedPostAttribute("meta");
     let ba = "";
     const googleFontsAttr =
-      ":100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic";
+      ":100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic&display=swap";
     const link = document.createElement("link");
     link.rel = "stylesheet";
 
