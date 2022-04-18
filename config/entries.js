cf5a5b7c999d36201bd0b398c10abc737cb57971
Monir <fencemonir@gmail.com>
2022-04-18 14:04:05 +0600

    fixed icon undefined issues

--- config/entries.js
+++ config/entries.js
@@ -8,6 +8,7 @@ export { default as ResponsiveDimensionsControl } from "../controls/src/controls
 export { default as ResponsiveRangeController } from "../controls/src/controls/responsive-range-control";
 export { default as TypographyDropdown } from "../controls/src/controls/typography-control-v2";
 export { default as ColorControl } from "../controls/src/controls/color-control";
+export { default as faIcons } from "../controls/src/extras/faIcons";
 import "../controls/src/group-controls";
 export { default as AdvancedControls } from "../controls/src/group-controls/components/advanced-controls";
 
--- dist/controls.asset.php
+++ dist/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '032860adfefd68cbc7920753e1227dbf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'f50e3310690595fc570c1557d39eb83b');
