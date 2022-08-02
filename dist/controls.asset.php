a0d86f5dd4cae59cf56d674da1553078f5a6498d
Monir <fencemonir@gmail.com>
2022-08-02 11:26:36 +0600

    update style handler and controls

--- dist/controls.asset.php
+++ dist/controls.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '2688b5c4852a984ed04ff8a69c169eb6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'd9b23b202b2af89eb0b229824e990592');
